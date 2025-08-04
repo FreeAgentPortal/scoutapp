36e58dbac6724729a6fdf076c2a4fb9d1de4502c
Austin Howard <cnfishead@hotmail.com>
2025-08-04 19:40:53 -0400

    ...

--- src/app/support/[id]/page.tsx
+++ src/app/support/[id]/page.tsx
@@ -12,6 +12,7 @@ export const metadata: Metadata = {
   },
 };
 
-export default function TicketDetailPage({ params }: { params: { id: string } }) {
-  return <TicketDetail ticketId={params.id} />;
+export default async function TicketDetailPage({ params }:{ params:  Promise<{ id: string }> }) {
+  const { id } = await params;
+  return <TicketDetail ticketId={id} />;
 }
